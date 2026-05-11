4882bda9908ce8b8ef1f396474930b0c9d7e565d
fatemeh <fatemeh@Fatemehs-MacBook-Air.local>
2026-05-11 15:18:19 +0200

    test ci workflow

--- services/vote/app.py
+++ services/vote/app.py
@@ -21,7 +21,7 @@ app.logger.setLevel(logging.INFO)
 
 def get_redis():
     if not hasattr(g, 'redis'):
-        g.redis = Redis(host=redis_host, port=redis_port, db=0, socket_timeout=6)
+        g.redis = Redis(host=redis_host, port=redis_port, db=0, socket_timeout=5)
     return g.redis
 
 @app.route("/", methods=['POST','GET'])
